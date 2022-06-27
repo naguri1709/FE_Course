94b0aaa29d8fa1adaf38d4a29383425c01b944d6
Đinh Hoàng Lộc <19520148@gm.uit.edu.vn>
2022-06-27 14:45:31 +0700

    last commit

---
+++ js_buoi_3/bai4.js
@@ -0,0 +1,49 @@
+// đổi first_name và last_name thành camelCase (firstName và lastName) 
+
+const obj = [
+  {
+    id: 1,
+    first_name: "Eamon",
+    last_name: "Harhoff",
+    email: "eharhoff0@imageshack.us",
+    gender: "Male",
+    age: 76,
+    salary: 18888,
+  },
+];
+
+const jsonData = require("./data.json");
+
+const toCamel = (s) => {
+  return s.replace(/([-_][a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace("-", "").replace("_", "");
+  });
+};
+
+const isArray = function (a) {
+  return Array.isArray(a);
+};
+
+const isObject = function (o) {
+  return o === Object(o) && !isArray(o) && typeof o !== "function";
+};
+
+const keysToCamel = function (o) {
+  if (isObject(o)) {
+    const n = {};
+
+    Object.keys(o).forEach((k) => {
+      n[toCamel(k)] = keysToCamel(o[k]);
+    });
+
+    return n;
+  } else if (isArray(o)) {
+    return o.map((i) => {
+      return keysToCamel(i);
+    });
+  }
+
+  return o;
+};
+
+console.log(keysToCamel(jsonData));
